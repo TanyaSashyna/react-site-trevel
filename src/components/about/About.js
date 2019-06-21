dfd066c17138e35518c2415516cd58291cc96d83
Tanya <sashina51@gmail.com>
2019-06-21 19:49:56 +0300

    added html on about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,7 +6,23 @@ export default class About extends React.Component {
     render() {
         return (
             <div className="main">
-                <h1>About page</h1>
+                <div className="picture">
+                    <img src="https://panch.com.ua/files/images/units/covers/big/panch20.jpg" alt="bus-picture"/>
+                    /* сделать картинку на фон */
+                    <h1 className="caption-page">О нас</h1>
+                </div>
+                <div className="caption-wrap">
+                    <h2 className="caption">Чем мы занимаемся</h2>
+                    <p className="legend">
+                        Пассажирские перевозки по Украина комфортабельными автобусами и микроавтобусами.
+                        Стоимость каждой поездки рассчитывается индивидуально.
+                        Для постоянных клиентов существует система скидок.
+                    </p>
+                </div>
+                <div className="lozung">Мы сделаем Ваши переезды комфортными</div>
+                <div className="picture">
+                    <img src="https://panch.com.ua/files/images/units/covers/big/panch20.jpg" alt="bus-picture"/>
+                </div>
             </div>
         )
     }
