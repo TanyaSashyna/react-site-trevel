721fd80087dbf2d719be118a19519cf7c946b384
Tanya Sashyna <sashina51@gmail.com>
2019-06-26 21:07:02 +0300

    fix footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -18,56 +18,38 @@ export default class Main extends React.Component {
                 <div className='container footer-wrap'>
                     <div className="footer-wrap__case nav-list">
                         <ul>
-                            {
-                                menu.map(el =>
-                                    <li key={el.id}>
-                                        <Link to={el.path} className='link'>
-                                            {el.text}
-                                        </Link>
-                                    </li>
-                                )
-                            }
+                            {menu.map(el =>
+                                <li key={el.id}>
+                                    <Link to={el.path} className='link'>
+                                        {el.text}
+                                    </Link>
+                                </li>
+                            )}
                         </ul>
                     </div>
                     <div className='footer-wrap__case'>
-                        <ContactItem classIcon="fas fa-mobile-alt" contactName="Телефон" scheduleText="(099) 000 00 00"/>
-                        <ContactItem classIcon="fas fa-envelope" contactName="E-mail" scheduleText="travel@gmail.com"/>
-                            {/*<div className='phone'>
-                            <div className="contacts-name">
-                                <i className="fas fa-mobile-alt"></i> Телефон:
-                            </div>
-                            <div className="footer-phone">
-                                (099) 000 00 00
-                            </div>
-                        </div>
-                        <div className="email">
-                            <div className="contacts-name">
-                                <i className="fas fa-envelope"></i> E-mail:
-                            </div>
-                            <div className="footer-schedule">
-                                travel@gmail.com
-                            </div>
-                        </div>*/}
+                        <ContactItem
+                            classIcon="fas fa-mobile-alt"
+                            contactName="Телефон"
+                            scheduleText="(099) 000 00 00"
+                        />
+                        <ContactItem
+                            classIcon="fas fa-envelope"
+                            contactName="E-mail"
+                            scheduleText="travel@gmail.com"
+                        />
                     </div>
                     <div className='footer-wrap__case'>
-                        <ContactItem classIcon="fas fa-map-marker-alt" contactName="Адрес" scheduleText="г. Харьков, Павловская площадь, д.6, метро пл.Конституции"/>
-                        <ContactItem classIcon="fas fa-clock" contactName="Время работы офиса" scheduleText="09:00 - 19:00 Пн-Вс"/>
-                            /*<div className='address'>
-                            <div className="contacts-name">
-                                <i className="fas fa-map-marker-alt"></i> Адрес:
-                            </div>
-                            <div className="footer-contacts">
-                                г. Харьков, Павловская площадь, 6,<br />метро "Конституции"
-                            </div>
-                        </div>
-                        <div className="time-work">
-                            <div className="contacts-name">
-                                <i className="fas fa-clock"></i> Время работы офиса:
-                            </div>
-                            <div className="footer-schedule">
-                                09:00 - 19:00 Пн-Вс
-                            </div>
-                        </div>*/}
+                        <ContactItem
+                            classIcon="fas fa-map-marker-alt"
+                            contactName="Адрес"
+                            scheduleText="г. Харьков, Павловская площадь, д.6, метро пл.Конституции"
+                        />
+                        <ContactItem
+                            classIcon="fas fa-clock"
+                            contactName="Время работы офиса"
+                            scheduleText="09:00 - 19:00 Пн-Вс"
+                        />
                     </div>
                 </div>
             </footer>
