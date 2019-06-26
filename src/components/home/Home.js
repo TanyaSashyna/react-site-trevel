f12ab495eec000d43007d470d7c67303fff49989
Tanya Sashyna <sashina51@gmail.com>
2019-06-26 21:53:28 +0300

    creating content on the home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
+import "./home.scss";
 
 export default class Home extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            regularRoutes: ['Харьков - Кириловка', 'Харьков - Скадовск', 'Харьков - Лазурное']
+        }
+    }
     render() {
+        const { regularRoutes } = this.state;
         return (
             <div className="main">
-                <h1>Home page</h1>
+                <div className="picture">
+                    <div className="caption-page">
+                        <div className="container home-caption">
+                            <h1>Пассажирские перевозки по Украине</h1>
+                            <Link to="/transfer" className="btn">
+                                Заказать
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container">
+                    <div className="home-about">
+                        <p>
+                            Автобусные пассажирские перевозки нашим комфортабельным автобусом доставят
+                            вам удовольствие от поездки. Время в пути пройдет незаметно благодаря
+                            уютной обстановке салона и приветливому водителю.
+                        </p>
+                    </div>
+                    <div className="home-routes">
+                        <h2>Регулярные маршруты</h2>
+                        <ul>
+                            {regularRoutes.map(
+                                (route, ind) => <li key={ind}>{route}</li>
+                            )}
+                        </ul>
+                    </div>
+                </div>
             </div>
         )
     }
