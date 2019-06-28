4098b0d27369723b65bd34754db1e4673a74d92e
Tanya <sashina51@gmail.com>
2019-06-28 17:31:13 +0300

    Update initial-state.js

--- src/reducer/initial-state.js
+++ src/reducer/initial-state.js
@@ -1,4 +1,47 @@
 export const initialState = {
+    dataForm: [
+        {
+            type: 'text',
+            name: 'firstName',
+            placeholder: 'Имя',
+            errorText: 'Введите имя',
+            labelText: 'Имя',
+            id: 1
+        },
+         {
+            type: 'text',
+            name: 'lastName',
+            placeholder: 'Фамилия',
+            errorText: 'Введите имя',
+            labelText: 'Фамилия',
+            id: 2
+        },
+        {
+            type: 'phone',
+            name: 'phone',
+            placeholder: 'Телефон',
+            errorText: 'Введите номер телефона',
+            labelText: 'Телефон',
+            id: 3
+        },
+        {
+            type: 'date',
+            name: 'date',
+            placeholder: 'Дата отправления',
+            errorText: 'Введите дату',
+            className: 'date',
+            labelText: 'Дата',
+            id: 4
+        },
+        {
+            type: 'text',
+            name: 'number',
+            placeholder: '0',
+            errorText: 'Не корректное количество мест',
+            labelText: 'Количество мест',
+            id: 5
+        }
+    ],
     showModalForm: false,
     userInfo: {
         fromTo: '',
