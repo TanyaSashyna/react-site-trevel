7cf98697cff27165c31b73be374c3e6d2e9114e0
Tanya <sashina51@gmail.com>
2019-06-28 17:22:38 +0300

    Update index.js

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -3,10 +3,12 @@ import burgerBtn from "./burger-btn";
 import transfer from "./transfer";
 import addGalleryPhoto from "./add-gallery-photo";
 import addContactInfo from "./add-contact-info";
+import mainMenu from "./main-menu";
 
 export default combineReducers({
     burgerBtn,
     transfer,
     addGalleryPhoto,
-    addContactInfo
+    addContactInfo,
+    mainMenu
 });
