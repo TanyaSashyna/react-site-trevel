4ab95f8d93ac949cbb511a78a258eea81068bc80
Tanya <sashina51@gmail.com>
2019-06-28 17:05:28 +0300

    Update index.js

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import burgerBtn from "./burger-btn";
 import transfer from "./transfer";
+import addGalleryPhoto from "./add-gallery-photo";
 
 export default combineReducers({
     burgerBtn,
-    transfer
+    transfer,
+    addGalleryPhoto
 });
