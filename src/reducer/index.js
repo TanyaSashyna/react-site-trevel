12e2f8c340e44c4f955a10a1897800b6aa9e1980
Tanya <sashina51@gmail.com>
2019-06-28 17:13:36 +0300

    index addContactInfo

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import burgerBtn from "./burger-btn";
 import transfer from "./transfer";
 import addGalleryPhoto from "./add-gallery-photo";
+import addContactInfo from "./add-contact-info";
 
 export default combineReducers({
     burgerBtn,
     transfer,
-    addGalleryPhoto
+    addGalleryPhoto,
+    addContactInfo
 });
